af9cb49fb2938d105ef2087afc8dc0ca219e38c4
Adam Telfer <adam@xmgstudio.com>
2012-03-22 16:55:50 -0400

    telf - updated gitignore
    Signed-off-by: Adam Telfer <adam@xmgstudio.com>

--- tweetagainst/build/tweetagainst.build/Debug-iphonesimulator/tweetagainst.build/Objects-normal/i386/ViewController.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/adamtelfer/workspace/tweet/tweetagainst/tweetagainst/ViewController.m \
-  /Users/adamtelfer/workspace/tweet/tweetagainst/tweetagainst/ViewController.h
--- tweetagainst/build/tweetagainst.build/Debug-iphonesimulator/tweetagainst.build/Objects-normal/i386/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/adamtelfer/workspace/tweet/tweetagainst/tweetagainst/main.m \
-  /Users/adamtelfer/workspace/tweet/tweetagainst/tweetagainst/AppDelegate.h
