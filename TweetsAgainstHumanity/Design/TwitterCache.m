9ef0fac107122252ff12de843a65dade5a3aba04
brandon <brandon@xmgstudio.com>
2012-03-23 12:20:28 -0400

    - Added in Authorized twitter request

--- TweetsAgainstHumanity/Design/TwitterCache.m
+++ TweetsAgainstHumanity/Design/TwitterCache.m
@@ -8,6 +8,7 @@
 
 #import "TwitterCache.h"
 #import <Twitter/Twitter.h>
+#import <Accounts/Accounts.h>
 
 @implementation TwitterCache
 
@@ -26,28 +27,75 @@ static TwitterCache* _cache;
 
 - (void) refresh 
 {
-    
-    NSURL* requestURL = [NSURL URLWithString:@"http://search.twitter.com/search.json?q=%23TAH_BLK"];
-    TWRequest* request = [[TWRequest alloc] initWithURL:requestURL parameters:nil requestMethod:TWRequestMethodGET];
-    [request performRequestWithHandler:
-     ^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
-         
-         if (responseData) {
-             //  Use the NSJSONSerialization class to parse the returned JSON
-             NSError *jsonError;
-             NSDictionary *timeline = [NSJSONSerialization JSONObjectWithData:responseData 
-                                             options:NSJSONReadingMutableLeaves 
-                                               error:&jsonError];
-             NSArray* results = [timeline objectForKey:@"results"];
-             if (results) {
-                 blackCards = results;
+    if ([TWTweetComposeViewController canSendTweet]) 
+    {
+        // Create account store, followed by a twitter account identifier
+        // At this point, twitter is the only account type available
+        ACAccountStore *account = [[ACAccountStore alloc] init];
+        ACAccountType *accountType = [account accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+        
+        // Request access from the user to access their Twitter account
+        [account requestAccessToAccountsWithType:accountType withCompletionHandler:^(BOOL granted, NSError *error) 
+         {
+             // Did user allow us access?
+             if (granted == YES)
+             {
+                 // Populate array with all available Twitter accounts
+                 NSArray *arrayOfAccounts = [account accountsWithAccountType:accountType];
+                 
+                 // Sanity check
+                 if ([arrayOfAccounts count] > 0) 
+                 {
+                     // Keep it simple, use the first account available
+                     ACAccount *acct = [arrayOfAccounts objectAtIndex:0];
+                     
+                     // Build a twitter request
+                     TWRequest *getRequest = [[TWRequest alloc] initWithURL:
+                                               [NSURL URLWithString:@"https://api.twitter.com/1/statuses/mentions.json?include_entities=true"] 
+                                                                  parameters:nil requestMethod:TWRequestMethodGET];
+                     
+                     // Post the request
+                     [getRequest setAccount:acct];
+                     NSLog(@"Twitter Request %@", [getRequest description]);
+                     
+                     
+                     // Block handler to manage the response
+                     [getRequest performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) 
+                      {
+                          NSLog(@"Twitter response, HTTP response: %i", [urlResponse statusCode]);
+
+                          
+                          // Testing - Json to take responeData and return string
+                          if (responseData) {
+                              //  Use the NSJSONSerialization class to parse the returned JSON
+                              NSError *jsonError;
+                              NSArray *results = [NSJSONSerialization JSONObjectWithData:responseData 
+                                                                                       options:NSJSONReadingMutableLeaves 
+                                                                                         error:&jsonError];
+                              
+                              // This will add black card messages upon refresh
+                              if (results) {
+                                  for (NSDictionary* tweet in results) {
+                                      NSString* message = [tweet objectForKey:@"text"];
+                                      if ([message rangeOfString:@"#tah"].location != NSNotFound) {
+                                          blackCards = message;
+                                          NSLog(@"Message added %@", [message description]);
+                                      }
+                                  }
+                                  blackCards = results;
+                              }
+                              else { 
+                                  // Inspect the contents of jsonError
+                                  NSLog(@"%@", jsonError);
+                              }
+                          }
+                          
+                          
+                      }];
+                 }
              }
-             else { 
-                 // Inspect the contents of jsonError
-                 NSLog(@"%@", jsonError);
-             }
-         }
-     }];
+         }];
+    }
     
 }
 
